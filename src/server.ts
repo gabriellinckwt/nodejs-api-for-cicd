583774d7e71c298ff9e26da5def2e54c1919f1f0
Gabriel Linck <gabriel.linck@poatek.com>
2024-07-25 21:27:13 -0300

    test for CI/CD

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,12 @@ import serverless from "serverless-http";
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import connectToDB from "./database/mysql/db";
+import serverConfig from "./config/server";
 const app = express();
 
 connectToDB();
 app.use(express.json());
 app.use(userRoutes);
-app.listen(3000);
+app.listen(serverConfig.PORT);
 
 export const handler = serverless(app);
