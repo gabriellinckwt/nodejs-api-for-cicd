d2431a7ff46f3ec58f841f74098c6bc58d62853a
Gabriel Linck <gabriel.linck@poatek.com>
2024-07-26 10:04:11 -0300

    update

--- src/controllers/UserController.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserRepository } from "../repositories/UserRepository";
-import { UserController } from "./UserController";
-
-describe("UserController", () => {
-  let userRepository = new UserRepository();
-  let userController = new UserController(userRepository);
-});
